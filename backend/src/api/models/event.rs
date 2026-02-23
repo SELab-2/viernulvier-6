3cc471fc90383505a3fb0fa753aec21c1f834e08
Hannes Klinckaert <hannes.klinckaert@ugent.be>
2026-02-23 20:09:32 +0100

    add event, genre and media api models

---
+++ backend/src/api/models/event.rs
@@ -0,0 +1,37 @@
+use chrono::{DateTime, Utc};
+use serde::Deserialize;
+
+use crate::api::models::localized_text::ApiLocalizedText;
+
+#[derive(Debug, Deserialize)]
+pub struct ApiEvent {
+    #[serde(rename = "@context")]
+    pub context: String,
+    #[serde(rename = "@id")]
+    pub id: String,
+    #[serde(rename = "@type")]
+    pub jsonld_type: String,
+
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
+    pub starts_at: DateTime<Utc>,
+    pub ends_at: DateTime<Utc>,
+    pub intermission_at: Option<DateTime<Utc>>,
+    pub doors_at: Option<DateTime<Utc>>,
+
+    pub box_office_id: String,
+    pub vendor_id: String,
+    pub max_tickets_per_order: u32,
+    pub uitdatabank_id: String,
+    pub secure: bool,
+    pub sms_verification: bool,
+
+    pub production: String,
+    pub status: String,
+    pub hall: String,
+    pub prices: Vec<String>,
+
+    pub info: ApiLocalizedText,
+    pub eticket_info: ApiLocalizedText,
+    pub external_order_url: ApiLocalizedText,
+}
---
+++ backend/src/api/models/media.rs
@@ -0,0 +1,44 @@
+use crate::api::models::localized_text::ApiLocalizedText;
+use chrono::{DateTime, Utc};
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct ApiMediaGallery {
+    #[serde(rename = "@context")]
+    pub context: String,
+    #[serde(rename = "@id")]
+    pub id: String,
+    #[serde(rename = "@type")]
+    pub r#type: String,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
+    pub name: String,
+    pub items: Vec<String>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ApiMediaItem {
+    #[serde(rename = "@context")]
+    pub context: String,
+    #[serde(rename = "@id")]
+    pub id: String,
+    #[serde(rename = "@type")]
+    pub jsonld_type: String,
+
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
+
+    #[serde(rename = "type")]
+    pub r#type: String,
+    pub original_filename: String,
+    pub position: u32,
+    pub width: u32,
+    pub height: u32,
+    pub format: String,
+    pub gallery: String,
+
+    pub title: ApiLocalizedText,
+    pub description: ApiLocalizedText,
+    pub credits: ApiLocalizedText,
+    pub link: ApiLocalizedText,
+}
