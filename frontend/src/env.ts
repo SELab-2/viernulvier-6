f9001ffe959d0573777b5388f7c521aad30256cc
Thomas Nelissen <thomas.nelissen@ugent.be>
2026-02-23 01:23:00 +0100

    Removed NEXT_PUBLIC_SITE_URL env var

--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -3,12 +3,10 @@ import { z } from "zod";
 
 export const env = createEnv({
     client: {
-        NEXT_PUBLIC_SITE_URL: z.string().url(),
         NEXT_PUBLIC_API_URL: z.string().url(),
         //NEXT_PUBLIC_GA_ID: z.string().optional()
     },
     runtimeEnv: {
-        NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
         NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
         //NEXT_PUBLIC_GA_ID: process.env.NEXT_PUBLIC_GA_ID
     }
