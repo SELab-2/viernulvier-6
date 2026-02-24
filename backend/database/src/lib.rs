fe756a15130aba31c04296694c69fb0880c8b350
Kaya-Sem <kayasem.vancauwenberghe@ugent.be>
2026-02-24 10:52:35 +0100

    add artist model

--- backend/database/src/lib.rs
+++ backend/database/src/lib.rs
@@ -3,6 +3,7 @@ use sqlx::{PgPool, postgres::PgPoolOptions};
 use crate::{error::DatabaseError, repos::user::UserRepo};
 
 pub mod models {
+    pub mod artist;
     pub mod event;
     pub mod production;
     pub mod user;
