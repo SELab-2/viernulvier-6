5d43647c8bed2bb3ee18f419fd5fc0c99f474215
Thomas Nelissen <thomas.nelissen@ugent.be>
2026-02-25 23:46:55 +0100

    feat: started experimenting with infinite canvas

--- frontend/src/app/[locale]/page.tsx
+++ frontend/src/app/[locale]/page.tsx
@@ -1,8 +1,18 @@
+"use client";
+
 import { Hero } from "@/components/sections";
+import { InfiniteCanvas } from "@/components/sections/infinite-canvas";
+import * as React from "react";
+import { MediaItem } from "@/components/sections/infinite-canvas/types";
+import manifest from "@/components/sections/infinite-canvas/artworks/manifest.json";
 
 export default function HomePage() {
+    const [media] = React.useState<MediaItem[]>(manifest);
+    const [textureProgress, setTextureProgress] = React.useState(0);
+
     return (
         <main className="h-screen overflow-hidden">
+            <InfiniteCanvas media={media} onTextureProgress={setTextureProgress} />
             <Hero />
         </main>
     );
