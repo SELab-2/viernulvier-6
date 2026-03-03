750e464256a9f07f7e31db8d3ffb3317008e7128
Hannes Klinckaert <hannes.klinckaert@ugent.be>
2026-03-03 15:08:01 +0100

    use o2o for easier from implementation

--- backend/src/dto/production.rs
+++ backend/src/dto/production.rs
@@ -1,10 +1,12 @@
 use database::{Database, models::production::Production};
+use o2o::o2o;
 use serde::Serialize;
 use uuid::Uuid;
 
 use crate::error::AppError;
 
-#[derive(Serialize)]
+#[derive(o2o, Serialize)]
+#[from_owned(Production)]
 pub struct ProductionPayload {
     pub id: Uuid,
 
@@ -60,50 +62,5 @@ impl ProductionPayload {
             .into_iter()
             .map(Self::from)
             .collect())
-    }
-}
-
-impl From<Production> for ProductionPayload {
-    fn from(value: Production) -> Self {
-        Self {
-            id: value.id,
-            slug: value.slug,
-            source_id: value.source_id,
-            supertitle_nl: value.supertitle_nl,
-            supertitle_en: value.supertitle_en,
-            title_nl: value.title_nl,
-            title_en: value.title_en,
-            artist_nl: value.artist_nl,
-            artist_en: value.artist_en,
-            meta_title_nl: value.meta_title_nl,
-            meta_title_en: value.meta_title_en,
-            meta_description_nl: value.meta_description_nl,
-            meta_description_en: value.meta_description_en,
-            tagline_nl: value.tagline_nl,
-            tagline_en: value.tagline_en,
-            teaser_nl: value.teaser_nl,
-            teaser_en: value.teaser_en,
-            description_nl: value.description_nl,
-            description_en: value.description_en,
-            description_extra_nl: value.description_extra_nl,
-            description_extra_en: value.description_extra_en,
-            description_2_nl: value.description_2_nl,
-            description_2_en: value.description_2_en,
-            video_1: value.video_1,
-            video_2: value.video_2,
-            quote_nl: value.quote_nl,
-            quote_en: value.quote_en,
-            quote_source_nl: value.quote_source_nl,
-            quote_source_en: value.quote_source_en,
-            programme_nl: value.programme_nl,
-            programme_en: value.programme_en,
-            info_nl: value.info_nl,
-            info_en: value.info_en,
-            description_short_nl: value.description_short_nl,
-            description_short_en: value.description_short_en,
-            eticket_info: value.eticket_info,
-            uitdatabank_theme: value.uitdatabank_theme,
-            uitdatabank_type: value.uitdatabank_type,
-        }
     }
 }
