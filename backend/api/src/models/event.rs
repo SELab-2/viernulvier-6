4fbf8adeae272f7fb825d75fe71ee87149fd9156
jerjoen <kockelbergjoeri@gmail.com>
2026-03-05 12:45:33 +0100

    implement From<ApiEvent> for event

--- backend/api/src/models/event.rs
+++ backend/api/src/models/event.rs
@@ -1,5 +1,7 @@
 use chrono::{DateTime, Utc};
+use database::models::event::EventCreate;
 use serde::Deserialize;
+use uuid::Uuid;
 
 use crate::models::localized_text::ApiLocalizedText;
 
@@ -34,4 +36,35 @@ pub struct ApiEvent {
     pub info: ApiLocalizedText,
     pub eticket_info: ApiLocalizedText,
     pub external_order_url: ApiLocalizedText,
+}
+
+impl From<ApiEvent> for EventCreate {
+    fn from(api: ApiEvent) -> Self {
+        let production_id = api
+            .production
+            .split('/')
+            .next_back()
+            .and_then(|s| Uuid::parse_str(s).ok())
+            .expect("invalid production id in event");
+
+        let hall_id = api
+            .hall
+            .split('/')
+            .next_back()
+            .and_then(|s| s.parse::<i32>().ok());
+
+        Self {
+            starts_at: api.starts_at,
+            ends_at: api.ends_at,
+            intermission_at: api.intermission_at,
+            doors_at: api.doors_at,
+            vendor_id: api.vendor_id,
+            box_office_id: api.box_office_id,
+            uitdatabank_id: api.uitdatabank_id,
+            max_tickets_per_order: api.max_tickets_per_order as i32,
+            production_id,
+            status: api.status,
+            hall: api.hall, // of hall_id als je dat liever hebt
+        }
+    }
 }
