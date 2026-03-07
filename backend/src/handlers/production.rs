eaceb4e7944931bda75a47afbb804dc65aee496c
Jarne Loos <jarne.loos@ugent.be>
2026-03-07 16:14:21 +0100

    remove unnecessary ProductionHandler

--- backend/src/handlers/production.rs
+++ backend/src/handlers/production.rs
@@ -3,13 +3,6 @@ use database::Database;
 
 use crate::{dto::production::ProductionPayload, error::AppError};
 
-pub struct ProductionHandler;
-
-impl ProductionHandler {
-    pub async fn all(db: Database) -> Result<Json<Vec<ProductionPayload>>, AppError> {
-        Ok(Json(ProductionPayload::all(&db, 10).await?))
-    }
-}
 
 #[utoipa::path(
     method(get),
@@ -21,5 +14,5 @@ impl ProductionHandler {
     )
 )]
 pub async fn all(db: Database) -> Result<Json<Vec<ProductionPayload>>, AppError> {
-    ProductionHandler::all(db).await
+    Ok(Json(ProductionPayload::all(&db, 10).await?))
 }
