cdb71db891dc42b5d622cd5ff4ff7c600c3d4b9f
Thomas Nelissen <thomas.nelissen@ugent.be>
2026-03-09 14:56:11 +0100

    fix: changed title and github url

--- docs/src/lib/layout.shared.tsx
+++ docs/src/lib/layout.shared.tsx
@@ -2,15 +2,15 @@ import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
 
 // fill this with your actual GitHub info, for example:
 export const gitConfig = {
-  user: 'fuma-nama',
-  repo: 'fumadocs',
+  user: 'SELab-2',
+  repo: 'viernulvier-6',
   branch: 'main',
 };
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: 'My App',
+      title: 'Viernulvier Archief',
     },
     githubUrl: `https://github.com/${gitConfig.user}/${gitConfig.repo}`,
   };
