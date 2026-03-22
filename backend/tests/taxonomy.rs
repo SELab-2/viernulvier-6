6710457ee6748d8b4b2f431b1c7d036bcee9423d
Kaya-Sem <kayasem.vancauwenberghe@ugent.be>
2026-03-22 13:25:53 +0100

    feat: add basic endpoint test

---
+++ backend/tests/taxonomy.rs
@@ -0,0 +1,72 @@
+use axum::http::StatusCode;
+use sqlx::PgPool;
+use viernulvier_api::dto::facet::FacetDto;
+
+use crate::common::{into_struct::IntoStruct, router::TestRouter};
+
+mod common;
+
+#[sqlx::test]
+#[test_log::test]
+async fn get_all(db: PgPool) {
+    let app = TestRouter::new(db);
+    let response = app.get("/taxonomy/facets").await;
+
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let data: Vec<FacetDto> = response.into_struct().await;
+    assert_eq!(data.len(), 4);
+
+    assert_eq!(data[0].slug, "discipline");
+    assert_eq!(data[1].slug, "format");
+    assert_eq!(data[2].slug, "theme");
+    assert_eq!(data[3].slug, "audience");
+
+    assert_eq!(data[0].tags.len(), 8); // discipline
+    assert_eq!(data[1].tags.len(), 8); // format
+    assert_eq!(data[2].tags.len(), 5); // theme
+    assert_eq!(data[3].tags.len(), 4); // audience
+
+    assert_eq!(data[0].tags[0].slug, "theatre");
+    assert_eq!(data[0].tags[7].slug, "installation");
+}
+
+#[sqlx::test]
+#[test_log::test]
+async fn get_filtered_by_production(db: PgPool) {
+    let app = TestRouter::new(db);
+    let response = app.get("/taxonomy/facets?entity_type=production").await;
+
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let data: Vec<FacetDto> = response.into_struct().await;
+    assert_eq!(data.len(), 4);
+    assert_eq!(data[0].slug, "discipline");
+    assert_eq!(data[3].slug, "audience");
+}
+
+#[sqlx::test]
+#[test_log::test]
+async fn get_filtered_by_artist(db: PgPool) {
+    let app = TestRouter::new(db);
+    let response = app.get("/taxonomy/facets?entity_type=artist").await;
+
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let data: Vec<FacetDto> = response.into_struct().await;
+    assert_eq!(data.len(), 1);
+    assert_eq!(data[0].slug, "discipline");
+    assert_eq!(data[0].tags.len(), 8);
+}
+
+#[sqlx::test]
+#[test_log::test]
+async fn get_filtered_by_unknown_entity_type(db: PgPool) {
+    let app = TestRouter::new(db);
+    let response = app.get("/taxonomy/facets?entity_type=nonexistent").await;
+
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let data: Vec<FacetDto> = response.into_struct().await;
+    assert_eq!(data.len(), 0);
+}
