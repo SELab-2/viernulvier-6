140104c7dfdb7b45e988f43d02fbe1ee7c48f1f0
Kaya-Sem <kayasem.vancauwenberghe@ugent.be>
2026-03-25 21:17:03 +0100

    chore: add migrations

---
+++ backend/migrations/20260325000000_collections_extend.down.sql
@@ -0,0 +1,4 @@
+ALTER TABLE collections DROP COLUMN slug;
+
+-- Note: PostgreSQL does not support removing values from an enum.
+-- The 'artist' and 'location' values cannot be removed from collection_content_type.
