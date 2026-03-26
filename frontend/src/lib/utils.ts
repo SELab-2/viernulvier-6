72b2741f09dac40c497bc4d1f95d0df8b24d32e8
Kaya-Sem <kayasem.vancauwenberghe@ugent.be>
2026-03-26 19:01:28 +0100

    chore: update docs

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]): string {
     return twMerge(clsx(inputs));
 }
+
+export function getTranslation<T extends { languageCode: string }>(
+    translations: T[],
+    languageCode: string
+): T | undefined {
+    return translations.find((t) => t.languageCode === languageCode);
+}
