35840eca928ddf0da7fdcf7720087ebef950ffde
Kaya-Sem <kayasem.vancauwenberghe@ugent.be>
2026-03-26 18:40:49 +0100

    fix: use genereated types

--- frontend/src/hooks/api/useTaxonomy.ts
+++ frontend/src/hooks/api/useTaxonomy.ts
@@ -2,15 +2,14 @@ import { useQuery } from "@tanstack/react-query";
 
 import { api } from "@/lib/api-client";
 import { mapFacets } from "@/mappers/taxonomy.mapper";
-import { EntityType } from "@/types/api/taxonomy.api.types";
+import { FacetResponse, EntityType } from "@/types/api/taxonomy.api.types";
 import { Facet } from "@/types/models/taxonomy.types";
 
 import { queryKeys } from "./query-keys";
 
 const fetchFacets = async (entityType?: EntityType): Promise<Facet[]> => {
     const params = entityType ? { entity_type: entityType } : undefined;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const { data } = await api.get<any[]>("/taxonomy/facets", { params });
+    const { data } = await api.get<FacetResponse[]>("/taxonomy/facets", { params });
     return mapFacets(data);
 };
 
