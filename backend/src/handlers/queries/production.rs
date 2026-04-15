850c8020451ad2353eac648bcf45deef2a1d7c9c
Thomas Nelissen <19342391+Thoosje@users.noreply.github.com>
2026-04-15 10:28:07 +0200

    feat(swagger): updated swagger schemas

--- backend/src/handlers/queries/production.rs
+++ backend/src/handlers/queries/production.rs
@@ -9,16 +9,25 @@ use crate::handlers::queries::sort::Sort;
 #[owned_into(ProductionSearch)]
 pub struct ProductionSearchQuery {
     pub q: Option<String>,
+    #[param(value_type = String, required = false)]
     pub discipline: Option<String>,
+    #[param(value_type = String, required = false)]
     pub format: Option<String>,
+    #[param(value_type = String, required = false)]
     pub theme: Option<String>,
+    #[param(value_type = String, required = false)]
     pub audience: Option<String>,
+    #[param(value_type = String, required = false)]
     pub artist: Option<String>,
+    #[param(value_type = String, required = false)]
     pub location: Option<String>,
+    #[param(value_type = String, required = false)]
     pub date_from: Option<String>,
+    #[param(value_type = String, required = false)]
     pub date_to: Option<String>,
 
     #[owned_into(~.map(Into::into))] // map the query sort to the database sort
+    #[param(value_type = Sort, inline, required = false)]
     pub sort: Option<Sort>,
     pub after: Option<String>,
 }
