4857bba150f7dcb83d29a62d351a8e96d3916285
Kaya-Sem <kayasem.vancauwenberghe@ugent.be>
2026-04-21 20:15:27 +0200

    chore: set correct pointers

--- frontend/src/components/ui/dialog.tsx
+++ frontend/src/components/ui/dialog.tsx
@@ -57,7 +57,7 @@ function DialogContent({
             >
                 {children}
                 {showClose && (
-                    <DialogPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-none disabled:pointer-events-none">
+                    <DialogPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 cursor-pointer rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-none disabled:pointer-events-none">
                         <X className="size-4" />
                         <span className="sr-only">Close</span>
                     </DialogPrimitive.Close>
