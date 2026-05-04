c33041df3785c4cc89496c8c57ca14251fe8bf92
Thoosje <19342391+Thoosje@users.noreply.github.com>
2026-05-04 10:39:17 +0200

    feat(artist): added artist page and mansonry grid component (#384)
    * feat(artist): base of artist page
    
    * feat(mansonry-grid): abstracted mansonry grid as component and linked to artist page
    
    * fix: lint error
    
    * feat(mansonry): added loading skeleton
    
    * fix(tests): removed tests for refactored component
    
    ---------
    
    Co-authored-by: Kaya-Sem <kayasem.vancauwenberghe@ugent.be>

--- frontend/src/components/collections/index.ts
+++ frontend/src/components/collections/index.ts
@@ -1,4 +1,2 @@
-// frontend/src/components/collections/index.ts
 export { CollectionHeader } from "./CollectionHeader";
 export { CollectionGrid } from "./CollectionGrid";
-export { CollectionItemCard } from "./CollectionItemCard";
